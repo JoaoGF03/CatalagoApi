6938852552a29739f6e1ef4c9f0a973d9c917178
guilherme <joao.g.f@icloud.com>
2022-11-18 17:48:33 -0300

    Produto endpoints

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -7,6 +7,5 @@ public class Categoria
   public int CategoriaId { get; set; }
   public string? Nome { get; set; }
   public string? Descricao { get; set; }
-  [JsonIgnore]
   public ICollection<Produto>? Produtos { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -16,9 +16,6 @@ var app = builder.Build();
 #region CRUD Categoria 
 app.MapPost("/categorias", async (AppDbContext db, Categoria categoria) =>
 {
-  if (string.IsNullOrEmpty(categoria.Nome) || string.IsNullOrEmpty(categoria.Descricao))
-    return Results.BadRequest(new { message = "The Nome and Descricao are required." });
-
   var existingCategoria = await db.Categorias.FirstOrDefaultAsync(c => c.Nome == categoria.Nome);
   if (existingCategoria is not null)
     return Results.Conflict(new { message = "Categoria already exists" });
@@ -30,7 +27,6 @@ app.MapPost("/categorias", async (AppDbContext db, Categoria categoria) =>
   return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 })
 .Produces<Categoria>(StatusCodes.Status201Created)
-.Produces(StatusCodes.Status400BadRequest)
 .Produces(StatusCodes.Status409Conflict)
 .WithTags("Categorias");
 
@@ -53,13 +49,17 @@ app.MapGet("/categorias/{id:int}", async (AppDbContext db, int id) =>
 .Produces(StatusCodes.Status404NotFound)
 .WithTags("Categorias");
 
+app.MapGet("/categorias-produtos", async (AppDbContext db) =>
+{
+  return Results.Ok(await db.Categorias.Include(categoria => categoria.Produtos).ToListAsync());
+})
+.Produces<List<Categoria>>(StatusCodes.Status200OK)
+.WithTags("Categorias");
+
 app.MapPut("/categorias/{id:int}", async (AppDbContext db, int id, Categoria categoria) =>
 {
   if (id != categoria.CategoriaId)
     return Results.BadRequest(new { message = "Categoria id mismatch" });
-
-  if (string.IsNullOrEmpty(categoria.Nome) || string.IsNullOrEmpty(categoria.Descricao))
-    return Results.BadRequest(new { message = "The Nome and Descricao are required." });
 
   var categoriaDB = await db.Categorias.FindAsync(id);
   if (categoriaDB is null)
@@ -99,11 +99,128 @@ app.MapDelete("/categorias/{id:int}", async (AppDbContext db, int id) =>
 .WithTags("Categorias");
 #endregion
 
-if (app.Environment.IsDevelopment())
+#region CRUD Produto
+app.MapPost("/produtos", async (AppDbContext db, Produto produto) =>
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+  var categoria = await db.Categorias.FindAsync(produto.CategoriaId);
+  if (categoria is null)
+    return Results.NotFound(new { message = "Categoria not found" });
+
+  var existingProduto = await db.Produtos.FirstOrDefaultAsync(p => p.Nome == produto.Nome);
+  if (existingProduto is not null)
+    return Results.Conflict(new { message = "Produto already exists" });
+
+  db.Produtos.Add(produto);
+
+  await db.SaveChangesAsync();
+
+  return Results.Created($"/produtos/{produto.ProdutoId}", produto);
+})
+.Produces<Produto>(StatusCodes.Status201Created)
+.Produces(StatusCodes.Status404NotFound)
+.Produces(StatusCodes.Status409Conflict)
+.WithTags("Produtos");
+
+app.MapGet("/produtos", async (AppDbContext db) =>
+{
+  return Results.Ok(await db.Produtos.ToListAsync());
+})
+.Produces<List<Produto>>(StatusCodes.Status200OK)
+.WithTags("Produtos");
+
+app.MapGet("/produtos-por-pagina", async (AppDbContext db, int? numeroPagina, int? tamanhoPagina) =>
+{
+  numeroPagina = numeroPagina <= 0 ? 1 : numeroPagina ?? 1;
+  tamanhoPagina = tamanhoPagina <= 0 ? 10 : tamanhoPagina ?? 10;
+
+  var produtos = await db.Produtos
+    .Skip((numeroPagina.Value - 1) * tamanhoPagina.Value)
+    .Take(tamanhoPagina.Value)
+    .ToListAsync();
+
+  return Results.Ok(produtos);
+})
+.Produces<List<Produto>>(StatusCodes.Status200OK)
+.WithTags("Produtos");
+
+app.MapGet("/produtos/{id:int}", async (AppDbContext db, int id) =>
+{
+  var produto = await db.Produtos.FindAsync(id);
+  if (produto is not null)
+    return Results.Ok(produto);
+
+  return Results.NotFound(new { message = "Produto not found" });
+})
+.Produces<Produto>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.WithTags("Produtos");
+
+app.MapGet("/produtos/nome/{filter}", async (AppDbContext db, string filter) =>
+{
+  var produtos = await db.Produtos
+    .Where(produto => produto.Nome.ToLower().Contains(filter.ToLower()))
+    .ToListAsync();
+
+  return produtos.Count() > 0
+    ? Results.Ok(produtos)
+    : Results.NotFound(Array.Empty<Produto>());
+})
+.Produces<List<Produto>>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.WithTags("Produtos");
+
+app.MapPut("/produtos/{id:int}", async (AppDbContext db, int id, Produto produto) =>
+{
+  if (id != produto.ProdutoId)
+    return Results.BadRequest(new { message = "Produto id mismatch" });
+
+  var produtoDB = await db.Produtos.FindAsync(id);
+  if (produtoDB is null)
+    return Results.NotFound(new { message = "Produto not found" });
+
+  var existingProduto = await db.Produtos.FirstOrDefaultAsync(p => p.Nome == produto.Nome);
+  if (existingProduto is not null && existingProduto.ProdutoId != produto.ProdutoId)
+    return Results.Conflict(new { message = "Produto already exists" });
+
+  produtoDB.Nome = produto.Nome;
+  produtoDB.Descricao = produto.Descricao;
+  produtoDB.Preco = produto.Preco;
+  produtoDB.Imagem = produto.Imagem;
+  produtoDB.Estoque = produto.Estoque;
+  produtoDB.DataCompra = produto.DataCompra;
+  produtoDB.CategoriaId = produto.CategoriaId;
+
+  await db.SaveChangesAsync();
+
+  return Results.Ok(produtoDB);
+})
+.Produces<Produto>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status400BadRequest)
+.Produces(StatusCodes.Status404NotFound)
+.Produces(StatusCodes.Status409Conflict)
+.WithTags("Produtos");
+
+app.MapDelete("/produtos/{id:int}", async (AppDbContext db, int id) =>
+{
+  var produto = await db.Produtos.FindAsync(id);
+  if (produto is null)
+    return Results.NotFound(new { message = "Produto not found" });
+
+  db.Produtos.Remove(produto);
+
+  await db.SaveChangesAsync();
+
+  return Results.NoContent();
+})
+.Produces(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status404NotFound)
+.WithTags("Produtos");
+#endregion
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+if (app.Environment.IsDevelopment())
   app.UseDeveloperExceptionPage();
-}
 
 app.Run();
